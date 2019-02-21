6dd8c353545f631a350ba9c3244cd1712f652708
Max Goodman <max.goodman@flatironschool.com>
2019-02-21 14:32:25 -0500

    adding describe tests for testing scripts

--- app/src/tests/App.test.js
+++ app/src/tests/App.test.js
@@ -7,3 +7,10 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+//describe (each feature supposedly built)
+//(1) Lists users and eth balance on one row
+//(2) Lists transactions of the user 
+//(3) Uses infinite scroll and cursor-based pagination
+//(4) Transfer Eth button and form exists, stores the balances in cache, adds tx to appropriate users, pops up modal confirmation
+//(5) 
